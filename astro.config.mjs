8b6c6b6a7affc001dbd48304be1d799a8e062e6d
ArpaAP <arpa1705@gmail.com>
2026-02-17 20:00:56 +0900

    fix: external link 플러그인 적용되지 않는 문제 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,71 @@ export default defineConfig({
     react(),
     mdx({
       remarkPlugins: [remarkReadingTime, remarkMath],
-      rehypePlugins: [rehypeKatex],
+      rehypePlugins: [
+        rehypeKatex,
+        [
+          rehypeExternalLinks,
+          {
+            content: (element) => {
+              // Add external link icon
+              element.children.push({
+                type: "element",
+                tagName: "span",
+                properties: {
+                  className: ["external-link-icon", "inline-block", "ml-0.5"],
+                },
+                children: [
+                  {
+                    type: "element",
+                    tagName: "svg",
+                    properties: {
+                      xmlns: "http://www.w3.org/2000/svg",
+                      width: "14",
+                      height: "14",
+                      viewBox: "0 0 24 24",
+                      fill: "none",
+                      stroke: "currentColor",
+                      "stroke-width": "2",
+                      "stroke-linecap": "round",
+                      "stroke-linejoin": "round",
+                      class: "lucide lucide-external-link",
+                    },
+                    children: [
+                      {
+                        type: "element",
+                        tagName: "path",
+                        properties: {
+                          d: "M15 3h6v6",
+                        },
+                        children: [],
+                      },
+                      {
+                        type: "element",
+                        tagName: "path",
+                        properties: {
+                          d: "M10 14 21 3",
+                        },
+                        children: [],
+                      },
+                      {
+                        type: "element",
+                        tagName: "path",
+                        properties: {
+                          d: "M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6",
+                        },
+                        children: [],
+                      },
+                    ],
+                  },
+                ],
+              });
+              // Set target and rel
+              element.properties.target = "_blank";
+              element.properties.rel = ["noopener", "noreferrer"];
+            },
+          },
+        ],
+      ],
     }),
     process.env.NODE_ENV === "development" ? keystatic() : null,
   ].filter(Boolean),
@@ -29,69 +93,5 @@ export default defineConfig({
         dark: "github-dark-default",
       },
     },
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          content: (element) => {
-            // Add external link icon
-            element.children.push({
-              type: "element",
-              tagName: "span",
-              properties: {
-                className: ["external-link-icon", "inline-block", "ml-0.5"],
-              },
-              children: [
-                {
-                  type: "element",
-                  tagName: "svg",
-                  properties: {
-                    xmlns: "http://www.w3.org/2000/svg",
-                    width: "14",
-                    height: "14",
-                    viewBox: "0 0 24 24",
-                    fill: "none",
-                    stroke: "currentColor",
-                    "stroke-width": "2",
-                    "stroke-linecap": "round",
-                    "stroke-linejoin": "round",
-                    class: "lucide lucide-external-link",
-                  },
-                  children: [
-                    {
-                      type: "element",
-                      tagName: "path",
-                      properties: {
-                        d: "M15 3h6v6",
-                      },
-                      children: [],
-                    },
-                    {
-                      type: "element",
-                      tagName: "path",
-                      properties: {
-                        d: "M10 14 21 3",
-                      },
-                      children: [],
-                    },
-                    {
-                      type: "element",
-                      tagName: "path",
-                      properties: {
-                        d: "M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6",
-                      },
-                      children: [],
-                    },
-                  ],
-                },
-              ],
-            });
-            // Set target and rel
-            element.properties.target = "_blank";
-            element.properties.rel = ["noopener", "noreferrer"];
-          },
-        },
-      ],
-    ],
   },
 });
